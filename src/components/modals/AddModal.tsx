868754fb56c6dd2f17761506b4d040ff230207e9
andri febrian <andri.feb.26@gmail.com>
2023-03-29 16:43:53 +0700

    fix

--- src/components/modals/AddModal.tsx
+++ src/components/modals/AddModal.tsx
@@ -38,7 +38,6 @@ export const AddModal: React.FC<IAddModalProps> = (props) => {
 
   return (
     <div
-      data-cy="modal-add"
       data-te-modal-init
       className="fixed top-0 left-0 z-[1055] hidden h-full w-full overflow-y-auto overflow-x-hidden outline-none"
       id="addModal"
@@ -50,7 +49,10 @@ export const AddModal: React.FC<IAddModalProps> = (props) => {
         data-te-modal-dialog-ref
         className="pointer-events-none relative flex min-h-[calc(100%-1rem)] w-auto translate-y-[-50px] items-center opacity-0 transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:min-h-[calc(100%-3.5rem)] min-[992px]:max-w-[800px]"
       >
-        <div className="min-[576px]:shadow-[0_0.5rem_1rem_rgba(#000, 0.15)] pointer-events-auto relative flex w-full flex-col rounded-md border-none bg-white bg-clip-padding text-current shadow-lg outline-none dark:bg-neutral-600 px-4">
+        <div
+          data-cy="modal-add"
+          className="min-[576px]:shadow-[0_0.5rem_1rem_rgba(#000, 0.15)] pointer-events-auto relative flex w-full flex-col rounded-md border-none bg-white bg-clip-padding text-current shadow-lg outline-none dark:bg-neutral-600 px-4"
+        >
           <div className="flex flex-shrink-0 items-center justify-between rounded-t-md border-b-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
             <h5
               data-cy="modal-add-title"
--- src/components/modals/AlertActivity.tsx
+++ src/components/modals/AlertActivity.tsx
@@ -4,7 +4,6 @@ import { BsExclamationCircle } from "react-icons/bs";
 export const AlertActivity: React.FC = () => {
   return (
     <div
-      data-cy="modal-information"
       data-te-modal-init
       className="fixed top-0 left-0 z-[1055] hidden h-full w-full overflow-y-auto overflow-x-hidden outline-none"
       id="alertActivity"
@@ -16,7 +15,10 @@ export const AlertActivity: React.FC = () => {
         data-te-modal-dialog-ref
         className="pointer-events-none relative flex min-h-[calc(100%-1rem)] w-auto translate-y-[-50px] items-center opacity-0 transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:min-h-[calc(100%-3.5rem)] min-[576px]:max-w-[500px]"
       >
-        <div className="min-[576px]:shadow-[0_0.5rem_1rem_rgba(#000, 0.15)] pointer-events-auto relative flex w-full flex-col rounded-lg border-none bg-white bg-clip-padding text-current shadow-lg outline-none dark:bg-neutral-600">
+        <div
+          data-cy="modal-information"
+          className="min-[576px]:shadow-[0_0.5rem_1rem_rgba(#000, 0.15)] pointer-events-auto relative flex w-full flex-col rounded-lg border-none bg-white bg-clip-padding text-current shadow-lg outline-none dark:bg-neutral-600"
+        >
           <div
             className="relative flex-auto text-center  p-4"
             data-te-modal-body-ref
