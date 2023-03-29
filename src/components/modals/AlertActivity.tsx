64d2765924c3bf6f600839d7bd4c448cb5d4ff80
andri febrian <andri.feb.26@gmail.com>
2023-03-29 15:39:24 +0700

    fix modal icon

--- src/components/modals/AlertActivity.tsx
+++ src/components/modals/AlertActivity.tsx
@@ -12,17 +12,27 @@ export const AlertActivity: React.FC = () => {
       aria-hidden="true"
     >
       <div
+        data-cy="modal-information"
         data-te-modal-dialog-ref
         className="pointer-events-none relative flex min-h-[calc(100%-1rem)] w-auto translate-y-[-50px] items-center opacity-0 transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:min-h-[calc(100%-3.5rem)] min-[576px]:max-w-[500px]"
       >
-        <div data-cy="modal-information" className="min-[576px]:shadow-[0_0.5rem_1rem_rgba(#000, 0.15)] pointer-events-auto relative flex w-full flex-col rounded-lg border-none bg-white bg-clip-padding text-current shadow-lg outline-none dark:bg-neutral-600">
+        <div className="min-[576px]:shadow-[0_0.5rem_1rem_rgba(#000, 0.15)] pointer-events-auto relative flex w-full flex-col rounded-lg border-none bg-white bg-clip-padding text-current shadow-lg outline-none dark:bg-neutral-600">
           <div
             className="relative flex-auto text-center  p-4"
             data-te-modal-body-ref
           >
             <div className="flex justify-start items-center gap-2 px-5">
-              <BsExclamationCircle data-cy="modal-information-icon" className="text-medium" size={24} />
-              <p data-cy="modal-information-title" className="text-[14px] font-medium">Activity berhasil dihapus</p>
+              <BsExclamationCircle
+                data-cy="modal-information-icon"
+                className="text-medium"
+                size={24}
+              />
+              <p
+                data-cy="modal-information-title"
+                className="text-[14px] font-medium"
+              >
+                Activity berhasil dihapus
+              </p>
             </div>
           </div>
         </div>
@@ -30,4 +40,3 @@ export const AlertActivity: React.FC = () => {
     </div>
   );
 };
-
