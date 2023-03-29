6180e93b30daa13ffdd0dc12306dc24b247bd747
andri febrian <andri.feb.26@gmail.com>
2023-03-29 16:40:29 +0700

    fix

--- src/components/modals/DeleteModal.tsx
+++ src/components/modals/DeleteModal.tsx
@@ -10,7 +10,6 @@ interface IDeleteModalProps {
 export const DeleteModal: React.FC<IDeleteModalProps> = (props) => {
   return (
     <div
-      data-cy="modal-delete"
       data-te-modal-init
       className="fixed top-0 left-0 z-[1055] hidden h-full w-full overflow-y-auto overflow-x-hidden outline-none"
       id="deleteModal"
@@ -22,7 +21,10 @@ export const DeleteModal: React.FC<IDeleteModalProps> = (props) => {
         data-te-modal-dialog-ref
         className="pointer-events-none relative flex min-h-[calc(100%-1rem)] w-auto translate-y-[-50px] items-center opacity-0 transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:min-h-[calc(100%-3.5rem)] min-[576px]:max-w-[500px]"
       >
-        <div className="min-[576px]:shadow-[0_0.5rem_1rem_rgba(#000, 0.15)] pointer-events-auto relative flex w-full flex-col rounded-lg border-none bg-white bg-clip-padding text-current shadow-lg outline-none dark:bg-neutral-600">
+        <div
+          data-cy="modal-delete"
+          className="min-[576px]:shadow-[0_0.5rem_1rem_rgba(#000, 0.15)] pointer-events-auto relative flex w-full flex-col rounded-lg border-none bg-white bg-clip-padding text-current shadow-lg outline-none dark:bg-neutral-600"
+        >
           <div
             className="relative flex-auto text-center  p-4"
             data-te-modal-body-ref
